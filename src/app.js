cbeffe8395928bc770909777a34933bf66edca92
Tom Hinton <tomjhinton@gmail.com>
2019-12-09 10:34:25 +0000

    back to pixel

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import '@tensorflow/tfjs-core'
 import '@tensorflow/tfjs-converter'
 
 import * as bodyPix from '@tensorflow-models/body-pix';
-
+console.log(bodyPix)
 const img = document.getElementById('image');
 
 const webcamElement = document.getElementById('webcam')
@@ -46,19 +46,39 @@ async function loadAndPredict() {
     async function draw(){
     const segmentation = await net.segmentPersonParts(webcamElement);
     const coloredPartImage = bodyPix.toColoredPartMask(segmentation);
-    const opacity = 0.7;
+
+    let opacity = Math.random()
     const flipHorizontal = false;
-    const maskBlurAmount = 0;
-    const pixelCellWidth = 10.0;
+    let maskBlurAmount = 10;
+    const pixelCellWidth = Math.random()*150
 
     bodyPix.drawPixelatedMask(
         canvas, webcamElement, coloredPartImage, opacity, maskBlurAmount,
         flipHorizontal, pixelCellWidth)
-      }
+
+
+        const maskBackground = true;
+  // Convert the segmentation into a mask to darken the background.
+  const foregroundColor = {r: 200, g: 0, b: 0, a: 0};
+  const backgroundColor = {r: 0, g: 0, b: 0, a: 255};
+  const backgroundDarkeningMask = bodyPix.toMask(
+      segmentation, foregroundColor, backgroundColor);
+
+   opacity = 0.7;
+   maskBlurAmount = 3
+
+  // Draw the mask onto the image on a canvas.  With opacity set to 0.7 and
+  // maskBlurAmount set to 3, this will darken the background and blur the
+  // darkened background's edge.
+  // bodyPix.drawMask(
+  //     canvas, canvas, backgroundDarkeningMask, opacity, maskBlurAmount, flipHorizontal)
+  //     }
+  //
+}
 setInterval(function () {
   draw()
 
-}, 10);
+}, 100);
 
 }
 loadAndPredict();
